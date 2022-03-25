37ce91526d6144da8ad6fc1e9b46122e4a1ca44c
yesu4658 <yesubabu.gudi@gmail.com>
2022-03-25 21:53:41 +0530

    added additional svg images

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script>
+    import { locationIcon1, locationIcon2 } from "./components/Images.svelte";
+
+    import TechStack from "./components/TechStack.svelte";
 </script>
 
 <main>
     <p>profile</p>
+    <img
+        src="https://avatars.githubusercontent.com/u/50919263?v=4"
+        alt="profile pic"
+        width="100"
+        height="100"
+    />
+    <div>
+        {@html locationIcon1()}
+    </div>
+
+    <TechStack />
 </main>
 
 <style>
---
+++ src/components/TechStack.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Icons } from "./Images.svelte";
+</script>
+
+<div>TechStack</div>
+{#each Icons as icon}
+    <span>{@html icon()}</span>
+{/each}
